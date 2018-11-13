24ce472fc7b724c37a735efae4fcd5a31925e484
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-13 16:22:43 +0900

    #2.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+const betweenHome = (req, res, next) => {
+  console.log("Between");
+  next();
+};
+
+app.use(betweenHome);
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
