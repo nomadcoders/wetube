c9f4cf06eadf63e5bdf937ceeee2652ac35eed44
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-13 15:51:31 +0900

    #2.4 Handling Routes with Express

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+function handleHome(req, res) {
+  res.send("Hello from home");
+}
+
+function handleProfile(req, res) {
+  res.send("You are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
