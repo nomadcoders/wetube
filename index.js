b928ea38311a0b8e597ccba8a9c89cd4c44b26fd
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-13 15:51:19 +0900

    #2.3 Your First Express Server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hi!");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
