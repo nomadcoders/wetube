0624caecafc48d6b3d6fb8abb40995be25132d04
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-14 16:29:51 +0900

    #2.16 Local Variables in Pug

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
---
+++ middlewares.js
@@ -0,0 +1,7 @@
+import routes from "./routes";
+
+export const localsMiddleware = (req, res, next) => {
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  next();
+};
