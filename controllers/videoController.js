1ddab2d7bcda5cbe94be1dee1609ed44a70d3d59
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-14 16:43:20 +0900

    #2.17 Template Variables in Pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,11 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "Search" });
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
