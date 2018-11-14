bb853a5e0922b8ba21dc5873b44570b568fb516a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-14 17:30:31 +0900

    #2.20 Change Profile HTML

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Cloning Youtube with Vanilla and NodeJS
 ## Pages:
 
 - [ ] Home
-- [ ] Join
-- [ ] Login
+- [x] Join
+- [x] Login
 - [x] Search
 - [ ] User Detail
 - [ ] Edit Profile
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,8 +9,8 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
 
 export default userRouter;
