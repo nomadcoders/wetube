319a0a56d35e5d3f91bb32809ed0c316dc03a351
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-15 16:20:12 +0900

    #3.3 Video Model

---
+++ models/Video.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+  fileUrl: {
+    type: String,
+    required: "File URL is required"
+  },
+  title: {
+    type: String,
+    required: "Tilte is required"
+  },
+  description: String,
+  views: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
+});
+
+const model = mongoose.model("Video", VideoSchema);
+export default model;
