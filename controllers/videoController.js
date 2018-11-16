2df6cfff8bc1bdd7cb12f0f59116fffcea50aaa0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-16 12:41:22 +0900

    #3.8 Getting Video by ID

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -34,8 +34,17 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
