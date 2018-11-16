3b00ff15ed77d04e177b44fe4cf548a376729b11
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-16 13:32:40 +0900

    #3.11 Installing ESLint

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,11 @@
 import routes from "../routes";
 import Video from "../models/Video";
 
+// Home
+
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ _id: -1 });
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
@@ -11,12 +13,16 @@ export const home = async (req, res) => {
   }
 };
 
+// Search
+
 export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy, videos });
+  res.render("search", { pageTitle: "Search", searchingBy });
 };
+
+// Upload
 
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
@@ -34,6 +40,8 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
+// Video Detail
+
 export const videoDetail = async (req, res) => {
   const {
     params: { id }
@@ -45,6 +53,8 @@ export const videoDetail = async (req, res) => {
     res.redirect(routes.home);
   }
 };
+
+// Edit Video
 
 export const getEditVideo = async (req, res) => {
   const {
@@ -71,12 +81,16 @@ export const postEditVideo = async (req, res) => {
   }
 };
 
+// Delete Video
+
 export const deleteVideo = async (req, res) => {
   const {
     params: { id }
   } = req;
   try {
     await Video.findOneAndRemove({ _id: id });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
   res.redirect(routes.home);
 };
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import routes from "../routes";
 import {
-  users,
   userDetail,
   editProfile,
   changePassword
