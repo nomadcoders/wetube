92f0912aa257b3f0c2b5b64dfebc02753bb5b56d
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-16 17:26:31 +0900

    #5.1 Footer and Login Join

--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.user = {
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1
   };
   next();
