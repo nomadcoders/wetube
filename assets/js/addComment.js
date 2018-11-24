ebe85d8585bc9bf7f2f5b75dc5ade6637f8e8e96
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-24 19:06:44 +0900

    #10.3 API Adding a Comment part Two

---
+++ assets/js/addComment.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+const addCommentForm = document.getElementById("jsAddComment");
+
+const sendComment = async comment => {
+  const videoId = window.location.href.split("/videos/")[1];
+  const response = await axios({
+    url: `/api/${videoId}/comment`,
+    method: "POST",
+    data: {
+      comment
+    }
+  });
+  console.log(response);
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const commentInput = addCommentForm.querySelector("input");
+  const comment = commentInput.value;
+  sendComment(comment);
+  commentInput.value = "";
+};
+
+function init() {
+  addCommentForm.addEventListener("submit", handleSubmit);
+}
+
+if (addCommentForm) {
+  init();
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
 import "../scss/styles.scss";
 import "./videoPlayer";
 import "./videoRecorder";
+import "./addComment";
