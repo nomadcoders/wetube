e561fd6918e72c573e73b3e753085362518b009f
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-24 17:24:12 +0900

    #9.0 Getting User Media

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,3 @@
 import "../scss/styles.scss";
 import "./videoPlayer";
+import "./videoRecorder";
---
+++ assets/js/videoRecorder.js
@@ -0,0 +1,26 @@
+const recorderContainer = document.getElementById("jsRecordContainer");
+const recordBtn = document.getElementById("jsRecordBtn");
+const videoPreview = document.getElementById("jsVideoPreview");
+
+const startRecording = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: { width: 1280, height: 720 }
+    });
+    videoPreview.srcObject = stream;
+    videoPreview.muted = true;
+    videoPreview.play();
+  } catch (error) {
+    recordBtn.innerHTML = "☹️ Cant record";
+    recordBtn.removeEventListener("click", startRecording);
+  }
+};
+
+function init() {
+  recordBtn.addEventListener("click", startRecording);
+}
+
+if (recorderContainer) {
+  init();
+}
