32e98acfe0f691f9d2748d8de7783b59a4114e34
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-24 19:19:05 +0900

    #10.4 API Adding a Comment part Three

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,5 +1,21 @@
 import axios from "axios";
+
 const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
+
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+};
+
+const addComment = comment => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  increaseNumber();
+};
 
 const sendComment = async comment => {
   const videoId = window.location.href.split("/videos/")[1];
@@ -10,7 +26,9 @@ const sendComment = async comment => {
       comment
     }
   });
-  console.log(response);
+  if (response.status === 200) {
+    addComment(comment);
+  }
 };
 
 const handleSubmit = event => {
