470e5e24bb42cd3f17f18555b5da5224ed5adc2e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-25 17:11:18 +0900

    #11.4 Building for Production

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import bodyParser from "body-parser";
 import passport from "passport";
 import mongoose from "mongoose";
 import session from "express-session";
+import path from "path";
 import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
@@ -22,8 +23,8 @@ const CokieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
-app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"));
+app.set("views", path.join(__dirname, "views"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "@babel/polyfill";
 import dotenv from "dotenv";
 import "./db";
 import app from "./app";
