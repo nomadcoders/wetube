d670eb5ff0209f82e4df83412716bb90b807deeb
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-11-25 17:37:58 +0900

    #11.6 Deploying to Heroku part Two

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose.connect(
-  process.env.MONGO_URL_PROD,
+  process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
   {
     useNewUrlParser: true,
     useFindAndModify: false
--- src/passport.js
+++ src/passport.js
@@ -15,7 +15,9 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: process.env.PRODUCTION
+        ? `https://polar-sea-27980.herokuapp.com${routes.githubCallback}`
+        : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
   )
